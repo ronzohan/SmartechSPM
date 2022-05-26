c701976f521ea1060aa7a39501f097adc468ffb7
Ron Daryl Magno <ron.zohan@gmail.com>
2022-05-26 11:08:50 +0800

    Update SPM formatting

--- Package.swift
+++ Package.swift
@@ -4,26 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SmartechSPM",
+    name: "Smartech",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SmartechSPM",
-            targets: ["SmartechSPM"]),
+            name: "Smartech",
+            targets: ["Smartech"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SmartechSPM",
-            dependencies: []),
-        .binaryTarget(name: <#T##String#>, path: <#T##String#>)
-        .testTarget(
-            name: "SmartechSPMTests",
-            dependencies: ["SmartechSPM"]),
+        .binaryTarget(name: "Smartech",
+                      url: "https://github.com/ronzohan/SmartechSPM/releases/download/3.2.7/Smartech.xcframework.zip",
+                     checksum: "f2c12ad19fc30e1c5a33b17c169ef7b4c639cfd7e3cfb2a0faee7880bc6b352b")
     ]
 )
