27339ab186d342944f3c7a502c79b0a08c600126
Ron Daryl Magno <ron.zohan@gmail.com>
2022-05-26 11:27:41 +0800

    Add sample file

--- Package.swift
+++ Package.swift
@@ -4,18 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "Smartech",
+    name: "SmartechSPM",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Smartech",
-            targets: ["Smartech"]),
+            name: "SmartechSPM",
+            targets: ["SmartechSPM", "Smartech"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        .target(name: "SmartechSPM"),
         .binaryTarget(name: "Smartech",
                       url: "https://github.com/ronzohan/SmartechSPM/releases/download/3.2.7/Smartech.xcframework.zip",
                      checksum: "f2c12ad19fc30e1c5a33b17c169ef7b4c639cfd7e3cfb2a0faee7880bc6b352b")
---
+++ Sources/SmartechSPM/SampleFile.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ron Daryl Magno on 5/26/22.
+//
+
+import Foundation
